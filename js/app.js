66b5054b99b0c0e948341cacbd001f92332f4d4f
Ken Pierce <kpierce8@gmail.com>
2016-01-14 23:17:46 -0800

    part 2 done

--- js/app.js
+++ js/app.js
@@ -2,6 +2,17 @@ var myApp = angular.module('myApp', ['ngRoute', 'firebase'])
 .constant('FIREBASE_URL', 'https://popping-heat-5540.firebaseio.com/');
 
 
+myApp.run(['$rootScope', '$location', 
+	function($rootScope, $location){
+		$rootScope.$on('$routeChangeError', 
+			function(event, next, previous, error){
+				if(error=='AUTH_REQUIRED'){
+					$rootScope.message = "Dude login already";
+					$location.path('/login');
+				}
+		});
+	}]);
+
 myApp.config(['$routeProvider', function($routeProvider){
 	$routeProvider.
 		when('/login', {
@@ -14,7 +25,12 @@ myApp.config(['$routeProvider', function($routeProvider){
 		}).
 		when('/success', {
 			templateUrl: 'views/success.html',
-			controller: 'SuccessController'
+			controller: 'SuccessController',
+			resolve: {
+				currentAuth: function(Authentication){
+					return Authentication.requireAuth();
+				}
+			}
 		}).
 		otherwise({
 			redirectTo: '/login'
--- js/services/authentication.js
+++ js/services/authentication.js
@@ -1,9 +1,19 @@
-myApp.factory('Authentication', ['$rootScope', '$firebaseAuth', '$location', 'FIREBASE_URL', 
-	function($rootScope, $firebaseAuth, $location, FIREBASE_URL){
+myApp.factory('Authentication', ['$rootScope', '$firebaseAuth', '$location', '$firebaseObject', 'FIREBASE_URL', 
+	function($rootScope, $firebaseAuth, $location, $firebaseObject, FIREBASE_URL){
 		var ref = new Firebase(FIREBASE_URL);
 		var auth = $firebaseAuth(ref);
 
-		return {
+			auth.$onAuth(function(authUser){
+				if (authUser){
+					var userRef = new Firebase(FIREBASE_URL + 'users/' + authUser.uid);
+					var userObj = $firebaseObject(userRef);
+					$rootScope.currentUser = userObj;
+				} else {
+					$rootScope.currentUser = '';
+				}
+			});
+
+		var myObject = {
 			login: function(user){
 				auth.$authWithPassword({
 					email: user.email,
@@ -15,6 +25,15 @@ myApp.factory('Authentication', ['$rootScope', '$firebaseAuth', '$location', 'FI
 				});
 				$rootScope.message = "welcome " + user.firstname;
 			},
+
+			logout: function(){
+				return auth.$unauth();	
+			}, //logout
+
+			requireAuth: function(){
+				return auth.$requireAuth();
+			}, //prevents activating route without being authenticated
+
 
 			register: function(user) {
 				auth.$createUser({
@@ -31,12 +50,15 @@ myApp.factory('Authentication', ['$rootScope', '$firebaseAuth', '$location', 'FI
 				email: user.email
 			});
 
-					$rootScope.message = "You're in tube user " + user.firstname + "thanks for registering";
+					myObject.login(user);
+
 				}).catch(function(error){
 					$rootScope.message = error.message;
 			}); //creat user
 		}
 	};
+
+	return myObject;
 }]);//factory
 
 
